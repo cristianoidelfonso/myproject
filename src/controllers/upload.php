b1588c446ba99416a8fa99b74cefe8c770f83845
Cristiano Idelfonso <cristiano_idelfonso@hotmail.com>
2020-09-15 15:48:30 -0300

    Primeiro commit

--- src/controllers/upload.php
+++ src/controllers/upload.php
@@ -1,25 +1,84 @@
 <?php
-    var_dump($arquivo);
 
-/*  
-*$dir é o caminho da pasta onde você deseja que os arquivos sejam salvos. 
-*Neste exemplo, supondo que esta pagina esteja em public_html/upload/ 
-*os arquivos serão salvos em public_html/upload/imagens/ 
-*Obs.: Esta pasta de destino dos arquivos deve estar com as permissões de escrita habilitadas. 
-*/ 
+    // Se o usuário clicou no botão cadastrar efetua as ações
+    if (isset($_POST['cadastrar'])) {
+        
+        // Recupera os dados dos campos
+        $foto = $_FILES["foto"];
 
-// $dir = "imagens/"; 
-$dir = UPLOAD_PATH; 
+        echo '<pre>';
+        print_r($foto);
+        echo '</pre>';
+        
+        // Se a foto estiver sido selecionada
+        if (!empty($foto["name"])) {
+            
+            // Largura máxima em pixels
+            $largura = 150;
+            // Altura máxima em pixels
+            $altura = 180;
+            // Tamanho máximo do arquivo em bytes
+            $tamanho = 1000;
 
-// recebendo o arquivo multipart 
-$file = $_FILES["arquivo"]; 
-// Move o arquivo da pasta temporaria de upload para a pasta de destino 
-if (move_uploaded_file($file["tmp_name"], "$dir/".$file["name"])) { 
-    echo "Arquivo enviado com sucesso!"; 
-} 
-else { 
-    echo "Erro, o arquivo n&atilde;o pode ser enviado."; 
-}           
+            $error = array();
+
+            // Verifica se o arquivo é uma imagem
+            if(!preg_match("/^image\/(jpg|pjpeg|jpeg|png|gif|bmp)$/", $foto["type"])){
+                $error[1] = "Isso não é uma imagem.";
+            } 
+        
+            // Pega as dimensões da imagem
+            $dimensoes = getimagesize($foto["tmp_name"]);
+        
+            // Verifica se a largura da imagem é maior que a largura permitida
+            // if($dimensoes[0] > $largura) {
+            //     $error[2] = "A largura da imagem não deve ultrapassar ".$largura." pixels";
+            // }
+
+            // Verifica se a altura da imagem é maior que a altura permitida
+            // if($dimensoes[1] > $altura) {
+            //     $error[3] = "Altura da imagem não deve ultrapassar ".$altura." pixels";
+            // }
+            
+            // Verifica se o tamanho da imagem é maior que o tamanho permitido
+            // if($foto["size"] > $tamanho) {
+            //         $error[4] = "A imagem deve ter no máximo ".$tamanho." bytes";
+            // }
+
+            // Se não houver nenhum erro
+            if (count($error) == 0) {
+            
+                // Pega extensão da imagem
+                preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $foto["name"], $ext);
+                
+                // Gera um nome único para a imagem
+                $nome_imagem = md5(uniqid(time())) . "." . $ext[1];
+                echo $nome_imagem . '<br>';
+                
+                // Caminho de onde ficará a imagem
+                $caminho_imagem = "fotos/" . $nome_imagem;
+                echo $caminho_imagem . '<br>';
+
+                // Faz o upload da imagem para seu respectivo caminho
+                move_uploaded_file($foto["tmp_name"], $caminho_imagem);
+            
+                // Insere os dados no banco
+                //$sql = mysql_query("INSERT INTO usuarios VALUES ('', '".$nome."', '".$email."', '".$nome_imagem."')");
+            
+                // Se os dados forem inseridos com sucesso
+                // if ($sql){
+                //     echo "Você foi cadastrado com sucesso.";
+                // }
+            }
+        
+            // Se houver mensagens de erro, exibe-as
+            if (count($error) != 0) {
+                foreach ($error as $erro) {
+                    echo $erro . "<br />";
+                }
+            }
+        }
+    }
 
 ?>
-<div class="message"><a class="btn btn-lg btn-primary" href="alunos.php">Voltar</a></div>
+<div class="message"><a class="btn btn-lg btn-primary" href="add_arquivo.php">Voltar</a></div>
--- src/views/add_arquivo.php
+++ src/views/add_arquivo.php
@@ -1,3 +1,26 @@
+<style>
+    .formularios{
+        padding: 5px 5px;
+        /* background-color: #E0E0E0 ;*/
+        font-weight: 700;
+        margin: 0 10px;
+	}
+    .botao{
+        text-align: center;
+        margin: 5px 0;
+    }
+    .background{
+        background-color: #379d5b78;
+        padding: 05px 15px ;
+        border-radius: .5rem;
+    }
+    .btn-primary, .btn-primary:hover, .btn-secondary, .btn-secondary:hover {
+        color: #fff;
+        background-color: #379d5b;
+        border-color:#379d5b;
+    }
+</style>
+
 <main class="content bg-info"><!-- Tela que apresenta todos os cursos -->
     <?php
         requireValidSession(true);
@@ -6,17 +29,20 @@
         
         include(TEMPLATE_PATH . "/messages.php");
     ?>
-    <div class="message"><a class="btn btn-lg btn-primary" href="#">Add</a></div>
-    <div class="message"><a class="btn btn-lg btn-primary" href="alunos.php">Voltar</a></div>
-    <div class="scroll"><?php //loadView('tabela_curso', ['cursos' => $cursos]); ?></div>
+    <!-- <div class="message"><a class="btn btn-lg btn-primary" href="#">Add</a></div> -->
+    <!-- <div class="message"><a class="btn btn-lg btn-primary" href="alunos.php">Voltar</a></div> -->
+    <!-- <div class="scroll"><?php //loadView('tabela_curso', ['cursos' => $cursos]); ?></div> -->
 
-    <div class="content bg-danger">
-
-       <form action="upload.php" method="post" enctype="multipart/form-data">
-            Selecione o arquivo: <input type="file" name="arquivo" />
-            <input type="submit" value="Enviar"/>
-        </form>
-    
+    <div class="content">
+        <div class="container background">
+            <div class="row form-group">
+                <form enctype="multipart/form-data" action="upload.php" method="post" name="cadastro-arquivo">
+                    Foto de exibição:<br />
+                    <input type="file" name="foto" /><br /><br />
+                    <input type="submit" name="cadastrar" value="Cadastrar" />
+                </form>
+            </div>
+        </div>
     </div>
 
 </main>
