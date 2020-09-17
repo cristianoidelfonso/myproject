bb7448af1eeec45d7bc2aa18dc4c3279c67d111c
Cristiano Idelfonso <cristiano_idelfonso@hotmail.com>
2020-09-17 09:45:56 -0300

    Atualizando cadastro de fotos

--- src/controllers/upload.php
+++ src/controllers/upload.php
@@ -9,30 +9,36 @@
     // Se o usuário clicou no botão cadastrar efetua as ações
     if (isset($_POST['cadastrar'])) {
 
+        // Conexão com o banco de dados
+        $conn = new PDO("mysql:dbname=db_uaitec;host=localhost","root","") 
+                or die("Problemas na conexão com o banco de dados.");
+
+        // echo ($conn) ? 'Conexão OK!' : 'Falha na conexão';
+
         $aluno = Aluno::getOne(['codigo' => $id_aluno], 'codigo,nome,cpf,foto,dataNasc,sexo,nomeMae');
         $alunoData = $aluno->getValues();
+
+        var_dump($alunoData);
         
         // Recupera os dados dos campos
         $foto = $_FILES["foto"];
 
-        // echo '<pre>';
-        // print_r($foto);
-        // echo '</pre>';
+        echo '<pre>';
+        print_r($foto);
+        echo '</pre>';
         
         // Se a foto estiver sido selecionada
         if (!empty($foto["name"])) {
             
             // Largura máxima em pixels
-            $largura = 150;
+            $largura = 1500;
             // Altura máxima em pixels
-            $altura = 180;
+            $altura = 1800;
             // Tamanho máximo do arquivo em bytes
-            $tamanho = 1000;
-
+            $tamanho = 10000;
             $error = array();
-
             // Verifica se o arquivo é uma imagem
-            if(!preg_match("/^image\/(jpg|pjpeg|jpeg|png|gif|bmp)$/", $foto["type"])){
+            if(!preg_match("/^image\/(pjpeg|jpeg|jpg|png|gif|bmp)$/", $foto["type"])){
                 $error[1] = "Isso não é uma imagem.";
             } 
         
@@ -40,44 +46,58 @@
             $dimensoes = getimagesize($foto["tmp_name"]);
         
             // Verifica se a largura da imagem é maior que a largura permitida
-            // if($dimensoes[0] > $largura) {
-            //     $error[2] = "A largura da imagem não deve ultrapassar ".$largura." pixels";
-            // }
-
+            if($dimensoes[0] > $largura) {
+                $error[2] = "A largura da imagem não deve ultrapassar ".$largura." pixels";
+            }
             // Verifica se a altura da imagem é maior que a altura permitida
-            // if($dimensoes[1] > $altura) {
-            //     $error[3] = "Altura da imagem não deve ultrapassar ".$altura." pixels";
-            // }
+            if($dimensoes[1] > $altura) {
+                $error[3] = "Altura da imagem não deve ultrapassar ".$altura." pixels";
+            }
             
             // Verifica se o tamanho da imagem é maior que o tamanho permitido
             // if($foto["size"] > $tamanho) {
             //         $error[4] = "A imagem deve ter no máximo ".$tamanho." bytes";
             // }
-
             // Se não houver nenhum erro
             if (count($error) == 0) {
             
                 // Pega extensão da imagem
-                preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $foto["name"], $ext);
-                
+                preg_match("/\.(gif|bmp|png|jpg|pjpeg|jpeg){1}$/i", $foto["name"], $ext);
                 // Gera um nome único para a imagem
                 $nome_imagem = md5(uniqid(time())) . "." . $ext[1];
-                echo $nome_imagem . '<br>';
                 
                 // Caminho de onde ficará a imagem
-                $caminho_imagem = "fotos/" . $nome_imagem;
-                echo $caminho_imagem . '<br>';
+                //mkdir(dirname(__FILE__,2).'/uploads/fotos/',0755); // Criando o diretorio /uploads/fotos/
+                if (is_dir(dirname(__FILE__,2).'/uploads/fotos/')){
+                    chdir(dirname(__FILE__,2).'/uploads/fotos/'); // Acessa o diretorio /uploads/fotos/
+                    mkdir($alunoData['cpf'] .'/',0755); // Criando o diretorio /uploads/fotos/cpf_aluno/
+                }
+                
+                if (is_dir($alunoData['cpf'])) {
+                    chdir($alunoData['cpf']); // Acessa o diretorio /uploads/fotos/cpf_aluno/
+                }
 
+                chdir(dirname(__FILE__,3).'/public');
+
+                $caminho_imagem = dirname(__FILE__,2).'/uploads/fotos/'.$alunoData['cpf'].'/'.$nome_imagem;
+                
                 // Faz o upload da imagem para seu respectivo caminho
                 move_uploaded_file($foto["tmp_name"], $caminho_imagem);
-            
+                die;
                 // Insere os dados no banco
-                //$sql = mysql_query("INSERT INTO usuarios VALUES ('', '".$nome."', '".$email."', '".$nome_imagem."')");
-            
+                $stmt = $conn->prepare("INSERT INTO usuarios (id,nome,email,foto) VALUES (NULL,:NOME,:EMAIL,:NOME_IMAGEM)");
+
+                $stmt->bindParam(":NOME",$nome);
+                $stmt->bindParam(":EMAIL",$email);
+                $stmt->bindParam(":NOME_IMAGEM",$nome_imagem);
+                
                 // Se os dados forem inseridos com sucesso
-                // if ($sql){
-                //     echo "Você foi cadastrado com sucesso.";
-                // }
+                if ($stmt->execute()){
+                    echo "Você foi cadastrado com sucesso.";
+                    header("Refresh: 3;url=index.php");
+                }else{
+                    die(print_r($stmt->errorInfo(), true));
+                }
             }
         
             // Se houver mensagens de erro, exibe-as
